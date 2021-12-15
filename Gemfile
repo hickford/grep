73cbcb5b467528bf91cde9717e2ef7fdd06734d9
matthickford <mirth.hickford@gmail.com>
2021-12-15 23:44:24 +0000

    no email

--- Gemfile
+++ Gemfile
@@ -2,5 +2,3 @@ source 'https://rubygems.org'
 ruby '~> 2.7'
 gem 'sinatra'
 gem 'thin'
-gem "mail"
-gem 'pony'
--- grep.rb
+++ grep.rb
@@ -1,40 +1,10 @@
 require 'sinatra/base'
-require 'mail'
-require 'pony'
 require 'yaml'
 
 class Grep < Sinatra::Base
-    if not ENV['SENDGRID_USERNAME']
-        emailconfig = YAML.load(File.read('config/development.yml'))['email']
-    end
-
-    set :email_username, ENV['SENDGRID_USERNAME'] || emailconfig['username']
-    set :email_password, ENV['SENDGRID_PASSWORD'] || emailconfig['password']
-    set :email_address, ENV['CLOUDMAILIN_FORWARD_ADDRESS'] || emailconfig['address']
-    set :email_service, ENV['EMAIL_SERVICE'] || emailconfig['service']
-    set :email_domain, ENV['SENDGRID_DOMAIN'] || 'localhost.localdomain'
-      
     helpers do
         def find(pattern)
             open('scowl-60.txt').grep(pattern)
-        end
-        
-        def email(to,subject,body)
-          Pony.mail(
-          :from => settings.email_address,
-          :to => to,
-          :subject => subject,
-          :body => body,
-          :via => :smtp,
-          :via_options => { 
-            :port                 => '587', 
-            :address              => 'smtp.' + settings.email_service, 
-            :enable_starttls_auto => true, 
-            :user_name            => settings.email_username, 
-            :password             => settings.email_password, 
-            :authentication       => :plain, 
-            :domain               => settings.email_domain
-          })
         end
     end
     
@@ -47,19 +17,6 @@ class Grep < Sinatra::Base
         matches = find(Regexp.new(pattern.downcase))
         intro = matches.empty? ? "No matches for #{pattern.downcase}" : "Matches for #{pattern.downcase}:"
         intro + "\n" + matches.join
-    end
-
-    post '/incoming_mail' do
-        correspondent = params[:from]
-        subject = params[:subject].chomp.sub(/\s\W?EOM\W?$/i,'').chomp
-        body = params[:plain].chomp
-        # do something with mail
-        pattern = subject
-        matches = find(Regexp.new(pattern.downcase))
-        answer = matches.empty? ? "no matches :(" : matches.join
-        email(correspondent,"Re: #{subject}","#{answer}\nhttp://grep.herokuapp.com/")
-        content_type 'text/plain'
-        "Emailed #{matches.length} matches for #{pattern} to #{correspondent}"
     end
 
 end
